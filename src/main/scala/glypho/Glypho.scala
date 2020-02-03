e7bebbb81cd60f08f32504c6c36cd574cdbb53b5
Dash-Lambda <dashlambda@gmail.com>
2020-02-03 18:47:38 -0500

    Minor Tweaks

--- src/main/scala/glypho/Glypho.scala
+++ src/main/scala/glypho/Glypho.scala
@@ -1,6 +1,6 @@
 package glypho
 
-import common.{Config, Interpreter, PartialParser, RegexParser}
+import common.{Config, Interpreter, RegexParser}
 
 import scala.annotation.tailrec
 import scala.util.Try
@@ -8,74 +8,19 @@ import scala.util.Try
 object Glypho extends Interpreter{
   val name: String = "Glypho"
   
+  val normParse: RegexParser[Char] = RegexParser("(....)"){m => parseOne(m.group(1))}
+  
   def apply(config: Config)(progRaw: String): Try[Seq[Char] => LazyList[Char]] = {
-    trait State{
-      def next: State}
-    trait Cont{
-      def apply(stk: Vector[Int], inp: Seq[Char]): State}
-    
-    object HaltState extends State{
-      def next: State = HaltState}
-    case class OutState(c: Char, next: State) extends State
-    case class RunState(stk: Vector[Int], inp: Seq[Char], cc: Cont) extends State{
-      def next: State = cc(stk, inp)}
-    
-    object HaltCont extends Cont{
-      def apply(stk: Vector[Int], inp: Seq[Char]): State = HaltState}
-    case class SkipCont(i: Int, prog: Vector[Char], loops: Vector[Vector[Char]], cc: Cont) extends Cont{
-      def apply(stk: Vector[Int], inp: Seq[Char]): State = prog match{
-        case c +: cs => c match{
-          case '[' => RunState(stk, inp, SkipCont(i + 1, cs, loops, cc))
-          case ']' =>
-            if(i == 0) RunState(stk, inp, RunCont(cs, loops, cc))
-            else RunState(stk, inp, SkipCont(i - 1, cs, loops, cc))
-          case _ => RunState(stk, inp, SkipCont(i, cs, loops, cc))}}}
-    case class RunCont(prog: Vector[Char], loops: Vector[Vector[Char]], cc: Cont) extends Cont{
-      def apply(stk: Vector[Int], inp: Seq[Char]): State = prog match{
-        case c +: cs =>
-          lazy val nxc = RunCont(cs, loops, cc)
-          c match{
-            case 'n' => RunState(stk, inp, nxc)
-            case 'i' => RunState(inp.head.toInt +: stk, inp.tail, nxc)
-            case '>' => RunState(stk.tail :+ stk.head, inp, nxc)
-            case '\\' => stk match{
-              case a +: b +: ns => RunState(b +: a +: ns, inp, nxc)}
-            case '1' => RunState(1 +: stk, inp, nxc)
-            case '<' => RunState(stk.last +: stk.init, inp, nxc)
-            case 'd' => RunState(stk.head +: stk, inp, nxc)
-            case '+' => stk match{
-              case a +: b +: ns => RunState((a + b) +: ns, inp, nxc)}
-            case '[' =>
-              if(stk.head == 0) RunState(stk, inp, SkipCont(0, cs, loops, cc))
-              else RunState(stk, inp, RunCont(cs, cs +: loops, cc))
-            case 'o' => OutState(stk.head.toChar, RunState(stk.tail, inp, nxc))
-            case '*' => stk match{
-              case a +: b +: ns => RunState((a*b) +: ns, inp, nxc)}
-            case 'e' => stk match{
-              case a +: b +: c +: d +: ns =>
-                val nop = parseOne(Vector(a, b, c, d).map(_.toChar).mkString)
-                RunState(ns, inp, RunCont(nop +: cs, loops, cc))}
-            case '-' => RunState((-stk.head) +: stk.tail, inp, nxc)
-            case '!' => RunState(stk.tail, inp, nxc)
-            case ']' =>
-              if(stk.head == 0) RunState(stk, inp, RunCont(cs, loops.tail, cc))
-              else RunState(stk, inp, RunCont(loops.head, loops, cc))
-            case _ => RunState(stk, inp, nxc)}
-        case _ => HaltState}}
-    
     @tailrec
     def rdo(state: State): Option[(Char, State)] = state.next match{
       case OutState(c, nxt) => Some((c, nxt))
       case HaltState => None
       case nxt => rdo(nxt)}
     
-    Try{parse(progRaw)} map{initProg =>
-      def initState(inp: Seq[Char]): State = RunState(Vector(), inp, RunCont(initProg, Vector(), HaltCont))
-      inputs => LazyList.unfold(initState(inputs))(rdo)}}
-  
-  def parse(progRaw: String): Vector[Char] = {
-    val normParse = RegexParser[Char](raw"""(....)"""){m => parseOne(m.group(1))}
-    normParse.parseAll(progRaw)}
+    Try{normParse.parseAll(progRaw)} map{initProg =>
+      inputs => {
+        val initState: State = RunState(Vector(), inputs, RunCont(initProg, Vector(), HaltCont))
+        LazyList.unfold(initState)(rdo)}}}
   
   def parseOne(tok: String): Char = {
     @tailrec
@@ -101,4 +46,63 @@ object Glypho extends Interpreter{
       case "0122" => '!'
       case "0123" => ']'}
     toOp(normalize(tok))}
+  
+  trait State{
+    def next: State}
+  
+  object HaltState extends State{
+    def next: State = HaltState}
+  
+  case class OutState(c: Char, next: State) extends State
+  
+  case class RunState(stk: Vector[Int], inp: Seq[Char], cc: Cont) extends State{
+    def next: State = cc(stk, inp)}
+  
+  trait Cont{
+    def apply(stk: Vector[Int], inp: Seq[Char]): State}
+  
+  object HaltCont extends Cont{
+    def apply(stk: Vector[Int], inp: Seq[Char]): State = HaltState}
+  
+  case class SkipCont(i: Int, prog: Vector[Char], loops: Vector[Vector[Char]], cc: Cont) extends Cont{
+    def apply(stk: Vector[Int], inp: Seq[Char]): State = prog match{
+      case c +: cs => c match{
+        case '[' => RunState(stk, inp, SkipCont(i + 1, cs, loops, cc))
+        case ']' =>
+          if(i == 0) RunState(stk, inp, RunCont(cs, loops, cc))
+          else RunState(stk, inp, SkipCont(i - 1, cs, loops, cc))
+        case _ => RunState(stk, inp, SkipCont(i, cs, loops, cc))}}}
+  
+  case class RunCont(prog: Vector[Char], loops: Vector[Vector[Char]], cc: Cont) extends Cont{
+    def apply(stk: Vector[Int], inp: Seq[Char]): State = prog match{
+      case c +: cs =>
+        lazy val nxc = RunCont(cs, loops, cc)
+        c match{
+          case 'n' => RunState(stk, inp, nxc)
+          case 'i' => RunState(inp.head.toInt +: stk, inp.tail, nxc)
+          case '>' => RunState(stk.tail :+ stk.head, inp, nxc)
+          case '\\' => stk match{
+            case a +: b +: ns => RunState(b +: a +: ns, inp, nxc)}
+          case '1' => RunState(1 +: stk, inp, nxc)
+          case '<' => RunState(stk.last +: stk.init, inp, nxc)
+          case 'd' => RunState(stk.head +: stk, inp, nxc)
+          case '+' => stk match{
+            case a +: b +: ns => RunState((a + b) +: ns, inp, nxc)}
+          case '[' =>
+            if(stk.head == 0) RunState(stk, inp, SkipCont(0, cs, loops, cc))
+            else RunState(stk, inp, RunCont(cs, cs +: loops, cc))
+          case 'o' => OutState(stk.head.toChar, RunState(stk.tail, inp, nxc))
+          case '*' => stk match{
+            case a +: b +: ns => RunState((a*b) +: ns, inp, nxc)}
+          case 'e' => stk match{
+            case a +: b +: c +: d +: ns =>
+              val nop = parseOne(Vector(a, b, c, d).map(_.toChar).mkString)
+              RunState(ns, inp, RunCont(nop +: cs, loops, cc))}
+          case '-' => RunState((-stk.head) +: stk.tail, inp, nxc)
+          case '!' => RunState(stk.tail, inp, nxc)
+          case ']' =>
+            if(stk.head == 0) RunState(stk, inp, RunCont(cs, loops.tail, cc))
+            else RunState(stk, inp, RunCont(loops.head, loops, cc))
+          case _ => RunState(stk, inp, nxc)}
+      case _ => HaltState}}
 }
