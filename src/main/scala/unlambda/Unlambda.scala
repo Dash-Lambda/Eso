279a2b1afd27510ce5f93130b04f35d6d6ae5fb8
Dash-Lambda <dashlambda@gmail.com>
2020-02-20 15:01:39 -0500

    Unlambda Tweaks

--- src/main/scala/unlambda/Unlambda.scala
+++ src/main/scala/unlambda/Unlambda.scala
@@ -1,11 +1,12 @@
 package unlambda
 
-import common.{Config, EsoExcep, Interpreter}
-import parsers.{EsoParsed, EsoParser, PartialParser, DepthRecurParser}
+import common.{Config, Interpreter}
+import parsers.{DepthRecurParser, EsoParser, PartialParser}
 
 import scala.annotation.tailrec
 import scala.collection.immutable
-import scala.util.{Failure, Success, Try}
+import scala.util.control.TailCalls._
+import scala.util.Try
 
 object Unlambda extends Interpreter{
   val name: String = "Unlambda"
@@ -27,23 +28,6 @@ object Unlambda extends Interpreter{
       case '?' +: c +: cs => (FuncExpr(QUES(c)), cs)
       case f +: cs if funcMap.isDefinedAt(f) => (FuncExpr(funcMap(f)), cs)}}
   val unlParser: EsoParser[Vector[Char], Expr] = {
-    def recur(src: Vector[Char]): Option[(Vector[Char], Int, Int)] = src match{
-      case '`' +: cs => Some((cs, 0, 1))
-      case _ => None}
-    def collect(xs: Seq[Expr]): Expr = xs match{
-      case x +: y +: _ => AppExpr(x, y)}
-    DepthRecurParser(funcParser)(2)(recur)(collect)}
-  
-  def apply(config: Config)(progRaw: String): Try[Seq[Char] => LazyList[Char]] = {
-    @tailrec
-    def udo(state: State): Option[(Char, State)] = state match{
-      case EndState => None
-      case PrintState(c, nxt) => Some((c, nxt))
-      case _ => udo(state.step())}
-    parse(progRaw) map {prog =>
-      inputs => LazyList.unfold(EvalState(prog, EndCont, Env(None, inputs)): State)(udo)}}
-  
-  def parse(progRaw: String): Try[Expr] = {
     val toks = "ivkscdr@|e`.?#".toVector
     @tailrec
     def condition(src: Vector[Char], ac: Vector[Char] = Vector()): Vector[Char] = src match{
@@ -53,120 +37,111 @@ object Unlambda extends Interpreter{
       case c +: cs if toks.contains(c) => condition(cs, ac :+ c)
       case _ +: cs => condition(cs, ac)
       case _ => ac}
-    
-    unlParser(condition(progRaw.toVector)) match{
-      case EsoParsed(prog, _, _, _) => Success(prog)
-      case _ => Failure(EsoExcep("Invalid Unlambda Expression"))}}
+    def recur(src: Vector[Char]): Option[(Vector[Char], Int, Int)] = src match{
+      case '`' +: cs => Some((cs, 0, 1))
+      case _ => None}
+    def collect(xs: Seq[Expr]): Expr = xs match{
+      case x +: y +: _ => AppExpr(x, y)}
+    DepthRecurParser(funcParser)(2)(recur)(collect).withConditioning(src => condition(src))}
+  
+  def apply(config: Config)(progRaw: String): Try[Seq[Char] => LazyList[Char]] = unlParser(progRaw.toVector).toTry("Invalid Unlambda Expression") map {prog =>
+    inputs => LazyList.unfold(prog(EndCont, Env(None, inputs)))(nxt => nxt.result.resolve)}
   
   case class Env(cur: Option[Char], inp: Seq[Char]){
     def read: Env = inp match{
       case c +: cs => Env(Some(c), cs)
       case _ => Env(None, inp)}}
   
-  trait State{
-    def step(): State}
+  trait Ret{
+    def resolve: Option[(Char, TailRec[Ret])]}
   
   trait Expr{
-    def apply(cc: Cont, env: Env): State}
+    def apply(cc: Cont, env: Env): TailRec[Ret]}
   
   trait Cont{
-    def apply(f: Func, env: Env): State}
+    def apply(f: Func, env: Env): TailRec[Ret]}
   
   trait Func{
-    def apply(f: Func, cc: Cont, env: Env): State}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret]}
   
-  //States
-  object EndState extends State{
-    def step(): State = EndState}
+  //Returns
+  object HaltRet extends Ret{
+    def resolve: Option[(Char, TailRec[Ret])] = None}
   
-  case class EvalState(x: Expr, cc: Cont, env: Env) extends State{
-    def step(): State = x(cc, env)}
-  
-  case class AppState(x: Func, y: Func, cc: Cont, env: Env) extends State{
-    def step(): State = x(y, cc, env)}
-  
-  case class PrintState(c: Char, nxt: State) extends State{
-    def step(): State = nxt}
+  case class PrintRet(c: Char, nxt: TailRec[Ret]) extends Ret{
+    def resolve: Option[(Char, TailRec[Ret])] = Some((c, nxt))}
   
   //Expressions
   case class FuncExpr(f: Func) extends Expr{
-    def apply(cc: Cont, env: Env): State = cc(f, env)}
+    def apply(cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(f, env))}
   
   case class AppExpr(x: Expr, y: Expr) extends Expr{
-    def apply(cc: Cont, env: Env): State = EvalState(x, ExprCont(y, cc), env)}
+    def apply(cc: Cont, env: Env): TailRec[Ret] = tailcall(x(ExprCont(y, cc), env))}
   
   //Continuations
   object EndCont extends Cont{
-    def apply(f: Func, env: Env): State = EndState}
-  
-  case class FuncCont(x: Func, cc: Cont) extends Cont{
-    def apply(f: Func, env: Env): State = AppState(x, f, cc, env)}
+    def apply(f: Func, env: Env): TailRec[Ret] = done(HaltRet)}
   
   case class ExprCont(y: Expr, cc: Cont) extends Cont{
-    def apply(f: Func, env: Env): State = f match{
-      case D => cc(D1(y), env)
-      case _ => EvalState(y, FuncCont(f, cc), env)}}
+    def apply(f: Func, env: Env): TailRec[Ret] = f match{
+      case D => tailcall(cc(D1(y), env))
+      case _ => tailcall(y(FuncCont(f, cc), env))}}
+  
+  case class FuncCont(x: Func, cc: Cont) extends Cont{
+    def apply(f: Func, env: Env): TailRec[Ret] = tailcall(x(f, cc, env))}
   
   case class DCont(y: Func, cc: Cont) extends Cont{
-    def apply(f: Func, env: Env): State = AppState(f, y, cc, env)}
+    def apply(f: Func, env: Env): TailRec[Ret] = tailcall(f(y, cc, env))}
   
   //Functions
   object I extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(f, env)}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(f, env))}
   
-  object V extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(V, env)}
+  object V extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(V, env))}
   
   case class OUT(c: Char) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = PrintState(c, cc(f, env))}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = done(PrintRet(c, tailcall(cc(f, env))))}
   
   case class D1(x: Expr) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = EvalState(x, DCont(f, cc), env)}
-  
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(x(DCont(f, cc), env))}
   object D extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(D1(FuncExpr(f)), env)}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(D1(FuncExpr(f)), env))}
   
   case class S2(x: Func, y: Func) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = {
-      EvalState(
-        AppExpr(
-          AppExpr(
-            FuncExpr(x),
-            FuncExpr(f)),
-          AppExpr(
-            FuncExpr(y),
-            FuncExpr(f))),
-        cc, env)}}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = {
+      val expr = AppExpr(AppExpr(FuncExpr(x), FuncExpr(f)), AppExpr(FuncExpr(y), FuncExpr(f)))
+      tailcall(expr(cc, env))}}
   case class S1(x: Func) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(S2(x, f), env)}
-  object S extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(S1(f), env)}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(S2(x, f), env))}
+  object S extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(S1(f), env))}
   
   case class K1(x: Func) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(x, env)}
-  object K extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc(K1(f), env)}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(x, env))}
+  object K extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc(K1(f), env))}
   
   case class C1(cc1: Cont) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = cc1(f, env)}
-  object C extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = AppState(f, C1(cc), cc, env)}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(cc1(f, env))}
+  object C extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = tailcall(f(C1(cc), cc, env))}
   
-  object E extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = EndState}
+  object E extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = done(HaltRet)}
   
-  object AT extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = env.cur match{
-      case None => AppState(f, V, cc, env.read)
-      case _ => AppState(f, I, cc, env.read)}}
+  object AT extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = env.cur match{
+      case None => tailcall(f(V, cc, env.read))
+      case _ => tailcall(f(I, cc, env.read))}}
   
   case class QUES(c: Char) extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = env.cur match{
-      case Some(`c`) => AppState(f, I, cc, env)
-      case _ => AppState(f, V, cc, env)}}
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = env.cur match{
+      case Some(`c`) => tailcall(f(I, cc, env))
+      case _ => tailcall(f(V, cc, env))}}
   
-  object PIPE extends Func{
-    def apply(f: Func, cc: Cont, env: Env): State = env.cur match{
-      case Some(c) => AppState(f, OUT(c), cc, env)
-      case None => AppState(f, V, cc, env)}}
+  object PIPE extends Func {
+    def apply(f: Func, cc: Cont, env: Env): TailRec[Ret] = env.cur match{
+      case Some(c) => tailcall(f(OUT(c), cc, env))
+      case None => tailcall(f(V, cc, env))}}
 }
