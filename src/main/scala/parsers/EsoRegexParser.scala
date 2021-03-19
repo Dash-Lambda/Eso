36f697e6f17a07590b1a3beccb79206a0503918b
Dash-Lambda <dashlambda@gmail.com>
2021-03-19 00:09:09 -0400

    Parser Optimizations

--- src/main/scala/parsers/EsoRegexParser.scala
+++ src/main/scala/parsers/EsoRegexParser.scala
@@ -1,9 +1,8 @@
 package parsers
 
-import scala.collection.mutable
 import scala.util.matching.Regex
 
-case class EsoRegexParser(reg: Regex, memo: mutable.HashMap[String, EsoParseRes[String]] = mutable.HashMap()) extends EsoParser[String]{
+case class EsoRegexParser(reg: Regex) extends EsoParser[String]{
   def apply(inp: String): EsoParseRes[String] = {
     reg.findFirstMatchIn(inp) match{
       case Some(m) =>
